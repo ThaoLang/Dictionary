602087dd3e0bb12e42513cbc676ffd019c239c7a
ThaoLang <langthao200243@gmail.com>
2022-11-29 05:38:52 +0700

    feat: complete UI #feature 1,2

--- src/Interface.java
+++ src/Interface.java
@@ -1,8 +1,10 @@
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import java.awt.event.*;
 import java.io.IOException;
 import java.time.LocalDate;
-import java.util.ArrayList;
+import java.util.HashSet;
 
 /**
  * PACKAGE_NAME
@@ -29,31 +31,41 @@ public class Interface  extends JFrame{
     private JLabel Logo;
     private JLabel Intro;
     private JLabel DailySlang;
+    private JList slangList;
+    private JList defList;
+    private JButton addButton;
+    private JButton editButton;
+    private JButton deleteButton;
     private JList keyList;
     private LocalDate lastRandom=LocalDate.MIN;
     private String typeSearching="slang";
+    private String[] defs;
 
-    public Interface(){
+    private void loadInSlangScrollPane(String[] arr){
+        slangList.setListData(arr);
+    }
+
+    public Interface() {
         super("Home Page");
-        SlangWord dict=new SlangWord();
+        searchButton.setSize(10, 10);
+        SlangWord dict = new SlangWord();
         try {
             dict.init("slang.txt");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        String[] keys=dict.getSlang().keySet().toArray(new String[0]);
-        keyList=new JList<String>(keys);
+        String[] keys = dict.getSlang().keySet().toArray(new String[0]);
+        loadInSlangScrollPane(keys);
 
         dailySlangButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (lastRandom.compareTo(LocalDate.now())==0){
-                    JOptionPane.showMessageDialog(null,"The Slang for today's been released. Come back tomorrow to learn slang!");
-                }
-                else{
-                    String randomText= dict.randomSlang();
+                if (lastRandom.compareTo(LocalDate.now()) == 0) {
+                    JOptionPane.showMessageDialog(null, "The Slang for today's been released. Come back tomorrow to learn slang!");
+                } else {
+                    String randomText = dict.randomSlang();
                     DailySlang.setText(randomText);
-                    lastRandom=LocalDate.now();
+                    lastRandom = LocalDate.now();
                 }
 
             }
@@ -75,19 +87,137 @@ public class Interface  extends JFrame{
         typeSearch.addItemListener(new ItemListener() {
             @Override
             public void itemStateChanged(ItemEvent e) {
-                typeSearching=typeSearch.getSelectedIndex()==0?"slang":"definition";
+                typeSearching = typeSearch.getSelectedIndex() == 0 ? "slang" : "definition";
             }
         });
         searchButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (typeSearching=="slang"){
+                defList.setListData(new String[0]);
+                if (typeSearching == "slang") {
+                    if (dict.getSlang().containsKey(textSearch.getText())) {
+                        String[] keys = new String[]{textSearch.getText()};
+                        loadInSlangScrollPane(keys);
+                    } else {
+                        slangList.setListData(new String[0]);
+                    }
 
-                }
-                else{
+                } else {
+                    String[] keyWords = textSearch.getText().split("`");
+                    if (dict.getDefinition().containsKey(keyWords[0])) {
+                        HashSet<String> keys = dict.getDefinition().get(keyWords[0]);
+
+                        for (int i = 1; i < keyWords.length; i++) {
+                            for (String kw : dict.getDefinition().get(keyWords[i]))
+                                if (!keys.contains(kw)) {
+                                    keys.add(kw);
+                                }
+                        }
+                        loadInSlangScrollPane(keys.toArray(new String[0]));
+                    } else {
+                        slangList.setListData(new String[0]);
+                    }
+
 
                 }
             }
+        });
+        slangList.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                defs = dict.getSlang().get(slangList.getSelectedValue()).toArray(new String[0]);
+                defList.setListData(defs);
+            }
+        });
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                defList.setListData(new String[0]);
+                String[] keys = dict.getSlang().keySet().toArray(new String[0]);
+                loadInSlangScrollPane(keys);
+            }
+        });
+        resetButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    dict.init("original.txt");
+                } catch (IOException er) {
+                    throw new RuntimeException(er);
+                }
+            }
+        });
+        addButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JTextField newSlang = new JTextField();
+                JTextField newDef = new JTextField();
+                Object[] add = {
+                        "Slang", newSlang,
+                        "Definition", newDef
+                };
+                int result = JOptionPane.showConfirmDialog(null, add, "Add new slang", JOptionPane.OK_CANCEL_OPTION);
+
+                if (result == JOptionPane.OK_OPTION) {
+                    String key = newSlang.getText();
+                    String value = newDef.getText();
+
+                    if (dict.getSlang().containsKey(key)) {
+                        String[] options = {"Overwrite", "Add new definition", "Cancel"};
+                        int opt = JOptionPane.showOptionDialog(null, "The slang already exists. Overwrite the old " +
+                                "slang or add new definition to this slang?", "Exist Slang", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, null);
+                        if (opt == JOptionPane.YES_OPTION) {
+                            dict.overwriteSlang(key, value);
+                        } else if (opt == JOptionPane.NO_OPTION) {
+                            dict.duplicateSlang(key, value);
+                        }
+                    } else {
+                        dict.addSlang(key, value);
+                    }
+                }
+
+            }
+
+        });
+        editButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JTextField oldSlang = new JTextField();
+                JTextField newslang = new JTextField();
+                Object[] add = {
+                        "Old slang", oldSlang,
+                        "New slang", newslang
+                };
+                int result = JOptionPane.showConfirmDialog(null, add, "Edit a slang", JOptionPane.OK_CANCEL_OPTION);
+
+                if (result == JOptionPane.OK_OPTION) {
+                    String oldkey = oldSlang.getText();
+                    String newkey = newslang.getText();
+
+                    if (!dict.getSlang().containsKey(oldkey)) {
+                        JOptionPane.showMessageDialog(null, "The slang doesn't exist!");
+                    } else {
+                        dict.editSlang(oldkey, newkey);
+                    }
+                }
+            }
+        });
+        deleteButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String word=slangList.getSelectedValue().toString();
+                if (word==null){
+                    JOptionPane.showMessageDialog(null,"Please choose a slang to delete");
+                }
+                else{
+                    int result = JOptionPane.showConfirmDialog(null, "Are you sure you want to delete?", "Delete slang", JOptionPane.OK_CANCEL_OPTION);
+
+                    if (result == JOptionPane.OK_OPTION) {
+                        dict.deleteSlang(word);
+                    }
+                }
+            }
+
         });
     }
 
